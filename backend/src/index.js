8866d3b1785db20a58cb402d5cdfc9be4f5aca59
Boominathan <eboominathan@gmail.com>
2025-05-28 16:51:41 +0530

    login completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
-const app = express();
+import cookiesParser from "cookie-parser";
+
 dotenv.config();
+const app = express();
 app.use(express.json());
+app.use(cookiesParser());
+
 app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5001;
